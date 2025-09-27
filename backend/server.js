93c7c3a69ae7edb8a4b0ba13bcea341d0c9dabde
Anuragsingh-0302 <anuragsingh4006@gmail.com>
2025-09-27 22:49:05 +0530

    Fix: Improved email templates & dotenv config in backend, enhanced settings menu behavior in frontend

--- backend/server.js
+++ backend/server.js
@@ -43,7 +43,7 @@ app.get("/", (req, res) => {
 });
 
 // server listen
-const PORT = process.env.PORT || process.env.CLIENT_PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`✅ Server running on http://localhost:${PORT}`);
+  console.log(`✅ Server running on ${PORT}`);
 });
